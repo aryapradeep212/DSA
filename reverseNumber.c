a8c13dd071bae189c9cdfca1a82aa528cd1697bc
Arya Pradeep <104966324+aryapradeep212@users.noreply.github.com>
2023-07-02 13:42:39 +0530

    Create reverseNumber.c

---
+++ reverseNumber.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 5
+int queue[MAX], front=-1,rear=-1;
+void reverse(int choice){
+    if(front==-1&&rear==-1)
+        front=rear=0;
+    else{
+    while(choice>0){
+        rear++;
+        queue[rear]=choice%10;
+        choice=choice/10;
+    }
+    }
+    printf("Reverse is\n");
+    while(front<rear){
+        front++;
+        printf("%d",queue[front]);
+    }
+}
+void main(){
+    int choice;
+        printf("Enter the number: ");
+        scanf("%d",&choice);
+        reverse(choice);  
+}
