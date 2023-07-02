84e6ef933b2bf6fbe191410a786fa1a06c788508
Arya Pradeep <104966324+aryapradeep212@users.noreply.github.com>
2023-07-02 20:24:37 +0530

    Create creatingTree.c

---
+++ creatingTree.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    struct node* left;
+    int data;
+    struct node* right;
+};
+struct node* root=NULL;
+struct node* getNode(int value){
+    struct node* newnode;
+    newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->left=NULL;
+    newnode->data=value;
+    newnode->right=NULL;
+    return newnode;
+}
+struct node* insertNode(struct node* root,int value){
+    if(root==NULL)
+        root=getNode(value);
+    else if(value>=root->data)
+        root->right=insertNode(root->right,value);
+    else if(value<=root->data)
+        root->left=insertNode(root->left,value);
+    return root;
+}
+void inorder(struct node* root){
+    if(root==NULL)
+        return;
+    inorder(root->left);
+    printf("%d\t",root->data);
+    inorder(root->right);
+}
+void main(){
+    int choice,value;
+    while(1){
+        printf("Enter the choice: ");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:printf("Enter the value: ");
+            scanf("%d",&value);
+            root=insertNode(root,value);
+            break;
+        case 2:inorder(root);
+        break;
+        default:exit(1);
+        }
+    }
+}
